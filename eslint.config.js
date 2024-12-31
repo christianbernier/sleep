9bd8909e34c9e41dabed28019989f89c8202b0a7
Christian Bernier <christianbernier02@gmail.com>
2024-12-31 16:10:04 -0500

    fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,32 +2,40 @@ import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import ts from 'typescript-eslint';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default ts.config(
+export default [
+	// JavaScript recommended rules
 	js.configs.recommended,
-	...ts.configs.recommended,
+
+	// TypeScript recommended rules
+	ts.configs.recommended,
+
+	// Svelte recommended rules
 	...svelte.configs['flat/recommended'],
+
+	// Prettier rules (ensure this comes last to avoid conflicts)
 	prettier,
-	...svelte.configs['flat/prettier'],
+
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				...globals.node
-			}
+			},
+			parser: tsParser // Use TypeScript parser
 		}
 	},
 	{
 		files: ['**/*.svelte'],
-
 		languageOptions: {
 			parserOptions: {
-				parser: ts.parser
+				parser: tsParser
 			}
 		}
 	},
 	{
 		ignores: ['build/', '.svelte-kit/', 'dist/']
 	}
-);
+];
