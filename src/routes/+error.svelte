2246af49334889026ab42f9846fbed5c880f5d21
Christian Bernier <christianbernier02@gmail.com>
2024-12-31 16:19:02 -0500

    error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import HighlightBox from "$lib/components/HighlightBox.svelte";
+	import LinkButton from "$lib/components/LinkButton.svelte";
+
+
+	import Message from "$lib/components/Message.svelte";
+
+</script>
+
+<HighlightBox>
+  <h2>There was an error!</h2>
+  <Message message={`Error: ${$page.error?.message || 'Unknown error'}`} />
+  <LinkButton href='/'>Go home</LinkButton>
+</HighlightBox>
